9e4f42f454399eef6144a17e75ffadad0720ba02
Dan <dponce6@gmail.com>
2019-01-30 14:08:55 -0300

    Set amount fragment as the initial one

--- app/src/main/java/com/example/dan/paymentapp/MainActivity.java
+++ app/src/main/java/com/example/dan/paymentapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dan.paymentapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,11 +14,20 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        setInitialFragment();
     }
 
     private void setInitialFragment()
     {
+        // Get the fragment manager
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
+        fragmentTransaction.replace(R.id.container, getInitialFragment());
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    private Fragment getInitialFragment()
+    {
+        return AmountFragment.newInstance("", "");
     }
 }
